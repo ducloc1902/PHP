f66b461cba225eafb834ba618ffe5001d4778d4c
Nguyễn Đức Lộc - 60135998 <62890971+ducloc1902@users.noreply.github.com>
2021-10-26 22:58:14 +0700

    Delete Lab6 directory

--- Lab6/QLNV/connect.php
+++
@@ -1,9 +0,0 @@
-<?php # Script - mysqli_connect.php
-
-// This file contains the database access information. 
-// This file also establishes a connection to MySQL, 
-// selects the database, and sets the encoding.
-
-$conn=mysqli_connect("localhost",'root','','qlnv')or die ('không thê kết nối tới CSDL');
-mysqli_set_charset($conn,'utf8');
-?>
